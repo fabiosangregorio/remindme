c7bfc1695c95b364e6f71a0b51427d98aa3973e3
Fabio Sangregorio <fabio.sangregorio@gmail.com>
2018-11-18 14:31:11 +0100

    First commit

---
+++ bot.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import time
+import random
+import re
+import threading
+
+import telepot
+
+from secret import TELEGRAM_TOKEN as TOKEN
+
+import remindme
+
+
+# handle chat messages
+def on_chat_message(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    text = msg['text']
+    if content_type == 'text' and '!remindme' in text.lower():
+        remindme.add_reminder(bot, chat_id, msg)
+
+
+# main function
+def main():
+    t2 = threading.Thread(target=remindme.main, args=[bot], daemon=True)
+    t2.start()
+
+    bot.message_loop({'chat': on_chat_message})
+    print('\n\nListening ...')
+
+    # Keep the program running
+    while 1:
+        time.sleep(3)
+
+
+bot = telepot.Bot(TOKEN)
+
+if __name__ == "__main__":
+    main()
